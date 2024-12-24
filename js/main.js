273b66f81000a15d9101c63802eab7e92a5d29c4
Kazen272 <cadudps@gmail.com>
2024-12-24 15:09:58 -0300

    Final Commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 const telefoneInput = document.getElementById('telefone');
-IMask(telefoneInput, {
+
+const mask = IMask(telefoneInput, {
     mask: '(00) 00000-0000'
 });
+mask.updateValue();
 
-console.log('mask')
+let idContador = 1;
+
+function cadastraContato() {
+    //definição das variáveis
+    let contato = document.getElementById('contato').value.trim();
+    let telefone = document.getElementById('telefone').value.trim();
+    const tabelaBody = document.querySelector("tbody");
+    const novaLinha = document.createElement("tr");
+
+    //Define como os campos serão preenchidos
+    novaLinha.innerHTML = `
+    <td>${idContador++}</td>
+    <td>${contato}</td>
+    <td>${telefone}</td>`;
+
+    //insere os dados na tabela
+    tabelaBody.appendChild(novaLinha);
+
+    //Limpa os campos antes do modal ser fechado
+    document.getElementById('contato').value = "";
+    document.getElementById('telefone').value = "";
+
+    //Fecha o modal
+    const modal = bootstrap.Modal.getInstance(document.getElementById("cadastro-contato"));
+    modal.hide();
+
+}
